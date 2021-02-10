d0c6715f174ee4ad596b67eb5c48113e6c741c45
johnt <elbmirtjd@gmail.com>
2021-02-10 14:06:16 -0500

    removing unnecessary file final.json, simplified logic

--- index.js
+++ index.js
@@ -34,27 +34,13 @@ fetch(url, {
     console.error('Error:', error)
 })
 
-for (var i=0; i<movData.results.length; i++){
-    if (movData.results[i].name == actorFirstName + ' '+ actorLastName){
-        for(var j=0; j<movData.results[i].known_for.length; j++){
-            var movTitle = JSON.stringify(movData.results[i].known_for[j].title, null, 2)
-            var movSum = JSON.stringify(movData.results[i].known_for[j].overview, null, 2)
-            var movPoster = JSON.stringify(movData.results[i].known_for[j].poster_path, null, 2)
-            fs.writeFile('./final.json', movTitle, err => {
-                if(err) {
-                    console.log('Error writing file', err)
-                }
-                else{
-                    console.log('Successfully wrote to file...')
-                }
-            })
-        }
-    }
-}
-
 app.post('/actorsearch', (req, res) => {
     if(!req.body) res.status(400).send({message: 'wrong'})
-    res.status(200).send()
+    for (var i=0; i<movData.results.length; i++){
+        if (movData.results[i].name == actorFirstName + ' '+ actorLastName){
+            res.status(200).send(movData.results[i].known_for)
+        }
+    }
 })
 
 app.listen(PORT, () => {
