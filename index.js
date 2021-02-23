9fbed564a921228867661f82e5b608942f71b922
johnt <elbmirtjd@gmail.com>
2021-02-23 12:25:24 -0500

    cleaning up express server

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const app = express()
 const bp = require('body-parser')
-const PORT = 3000
+const PORT = 9000
 var search = require('./search.json')
-var movData = require('./data.json')
-var {getJSONData} = require('./func.js')
 var {storeSearch} = require('./func.js')
 
 app.use(bp())
-
-var url ='https://api.themoviedb.org/3/search/person?api_key=07a240cb378733627b949d3c46e885c3&language=en-US&query='+ search.first + '%20' + search.last 
 
 app.post('/actorsearch', (req, res) => {
     console.log('x')
@@ -24,9 +20,9 @@ app.post('/actorsearch', (req, res) => {
     }
 })
 
-app.get('/actorsearch/results', (req, res) =>{
+app.get('/actorsearch/:firstName/:lastName', (req, res) =>{
     console.log('x')
-    getJSONData(url, res, movData, search.first, search.last)
+    //getJSONData(url, res, movData, search.first, search.last)
 })
 
 
