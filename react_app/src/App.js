c623ba838df2cfd7679fbbd619f2511dfed1de71
johnt <elbmirtjd@gmail.com>
2021-02-23 16:19:50 -0500

    fixing bug and start layout edit

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,18 +1,13 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect, useCallback} from 'react'
 import './App.css';
 import fetch from 'node-fetch';
-//var movData = require('./data.json')
 
 function App() {
-  const [firstName, setFirstName] = useState()
-  const [lastName, setLastName] = useState()
+  const [name, setName] = useState('')
   const [movInfo, setMovInfo] = useState([])
-  const [isLoading, setLoading] = useState(true)
 
-  //var url ='https://api.themoviedb.org/3/search/person?api_key=07a240cb378733627b949d3c46e885c3&language=en-US&query='+ firstName + '%20' + lastName
-
-  useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/search/person?api_key=07a240cb378733627b949d3c46e885c3&language=en-US&query=${firstName}%20${lastName}`
+  const fetchData = useCallback(() => {
+    fetch(`https://api.themoviedb.org/3/search/person?api_key=07a240cb378733627b949d3c46e885c3&language=en-US&query=${name}`
       , {
       method:"GET",
       header: {
@@ -21,47 +16,39 @@ function App() {
     })
     .then(res=>res.json())
     .then(response => {
-      setMovInfo(response)
-      setLoading(false)
-      //for (var i=0; i<response.results.length; i++){
-        //if (response.results[i].name == firstName + ' '+ lastName){
-          //setMovInfo(response.results.known_for[0])
-        //}
-        //else{
-          //setMovInfo(null)
-        //}
-      //}
+      for (var i=0; i<response.results.length; i++){
+        if (response.results[i].name == name){
+          setMovInfo(response.results.known_for)
+        }
+        else{
+          setMovInfo(null)
+        }
+      }
     })
     .catch(error => console.log(error))
-  }, [movInfo])
+  },[name])
 
-  const handleChangeFirst = e => {
-    setFirstName(e.target.value)
-  }
-
-  const handleChangeLast = e => {
-    setLastName(e.target.value)
-  }
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
 
   return (
     <div className="App">
-      <h1>Actor Search Results</h1>
-      <input value={firstName} onChange={handleChangeFirst} />
-      <input value={lastName} onChange={handleChangeLast}/>
-      {isLoading && <p> Loading Search Results...</p>}
-      <button onClick={console.log(movInfo)}>Find Actor Info</button>
-      {movInfo && (
-        Object.keys(movInfo).length === 0
-          ? <p>No Results</p>
-          :<div>
-            {Object.entries(movInfo).map(([movInfo]) => (
-              <span style= {StyleSheet}>
-                {movInfo}
-                {' '}
-              </span>
-            ))}
-          </div>
-      )}
+      <header className="App-header">
+        <h1>
+          Search for an Actor
+        </h1>
+        <input value={name} onChange={e => setName(e.target.value)}/> 
+        <button type='button' onClick={fetchData}>Find Actor Info</button>
+        {console.log(movInfo)}
+      </header>
+      <main>
+        {movInfo &&
+          <blockquote>
+            "{movInfo && movInfo.results}"
+          </blockquote>
+        }
+      </main>
     </div>
   );
 }
