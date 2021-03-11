14dd309a6d65311186ee3fd99ffe1f3bc5ad589e
blaze768 <elbmirtjd@gmail.com>
2021-03-11 10:52:15 -0500

    updated display format, added framework for db connection

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const express = require('express')
 const app = express()
 const bp = require('body-parser')
-const PORT = 9000
-var search = require('./search.json')
-var {storeSearch} = require('./func.js')
+const mongoClient = require('mongodb');
+const PORT = 5000
+const url = "mongodb://localhost:27017/";
+const actorName = "Tom Hardy"
+const id = "123456789"
+
+app.get('/', (req, res) =>{
+    console.log('x')
+    //getJSONData(url, res, movData, search.first, search.last)
+})
 
 app.use(bp())
 
 app.post('/actorsearch', (req, res) => {
     console.log('x')
     if(!req.body) res.status(400).send({message: 'no body'})
+    res.status(200).send('good');
     //query database
-    if(req.body.first === '' || req.body.last === ''){
-        res.send({message: 'please reenter search'})
-    }
-    else{
-        res.send({message: 'searching...'})
-        storeSearch(req.body)
-    }
+    mongoClient.connect(url, function(err){
+      if (err) throw err;
+      var dbo = db.db("actorsearch");
+      var myquery = {actorName};
+      var newvalues = { $set: {actor: actorName, actorID: id} };
+      dbo.collection("actorInfo").insertMany(myquery, newvalues, function(err, res) {
+        if (err) throw err;
+        console.log("Data inserted");
+        db.close();
+      })
+    })
 })
-
-app.get('/actorsearch/:firstName/:lastName', (req, res) =>{
-    console.log('x')
-    //getJSONData(url, res, movData, search.first, search.last)
-})
-
 
 app.listen(PORT, () => {
     console.log(`Running...`)
--- react_app/src/App.js
+++ react_app/src/App.js
@@ -5,6 +5,10 @@ import fetch from 'node-fetch';
 function App() {
   const [name, setName] = useState('')
   const [movInfo, setMovInfo] = useState([])
+  const mongoClient = require('mongodb').MongoClient;
+  const url = "mongodb://localhost:27017/";
+  const actorName = "Tom Hardy"
+  const id = "123456789"
 
   const fetchData = useCallback(() => {
     fetch(`https://api.themoviedb.org/3/search/person?api_key=07a240cb378733627b949d3c46e885c3&language=en-US&query=${name}`
@@ -19,7 +23,18 @@ function App() {
     .catch(error => console.log(error))
   },[name])
 
-  
+  //query database
+  mongoClient.connect(url, function(err, db){
+    if (err) throw err;
+    var dbo = db.db("actorsearch");
+    var myquery = {actorName};
+    var newvalues = { $set: {actor: actorName, actorID: id} };
+    dbo.collection("actorInfo").insertMany(myquery, newvalues, function(err, res) {
+      if (err) throw err;
+      console.log("Data inserted");
+      db.close();
+    })
+  })
 
   return (
     <div className="App">
